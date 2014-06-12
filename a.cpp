1575f725b4d133ec1633ad423846ad0d6564ee68
dubyoo <wangwei1543@gmail.com>
2014-06-12 23:22:18 +0800

    test2

---
+++ a.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: Dubyoo
+	> Mail:wangwei1543@gmail.com 
+	> Created Time: Thu 12 Jun 2014 11:20:22 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+int  main()
+{
+	cout << "hello test" << endl;
+	return 0;
+}
